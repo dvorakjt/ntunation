2dd3fa49d633a7c7fe9ec8f2e5ea83bc7bd212a3
Joseph Dvorak <dvorakjt@gmail.com>
2020-08-01 12:47:03 -0400

    updated get route

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ require('./routes/api-routes')(app);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
+app.get((req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
