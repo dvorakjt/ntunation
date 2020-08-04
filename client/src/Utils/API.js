3ff3de09263374fd164fb0bc7892c72b08487891
Joseph Dvorak <dvorakjt@gmail.com>
2020-08-04 16:39:19 -0400

    updated for heroku

--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -3,5 +3,8 @@ const axios = require('axios');
 export default {
     createUser: userData => {
         return axios.post('/api/users', userData);
+    },
+    updateUser: updatedData => {
+        return axios.put('/api/exercises', updatedData);
     }
 }
